ac38cdd9fdb143958e1b055de3cb0d090e68ae13
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 18:43:01 +0500

    optimazed carcounting

--- arma_complex_SQL/carcountimg.sql
+++ arma_complex_SQL/carcountimg.sql
@@ -1,6 +1,7 @@
-SELECT car.name, COUNT(car_image.id) AS image_count 
-FROM car 
-LEFT JOIN car_image ON car.id = car_image.car_id 
-GROUP BY car.name 
-ORDER BY image_count DESC 
+SELECT car.name, COUNT(car_image.id) AS image_count
+FROM car
+LEFT JOIN car_image ON car.id = car_image.car_id
+GROUP BY car.id, car.name
+ORDER BY image_count DESC
 LIMIT 5;
+
