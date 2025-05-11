303065f2d6901d0c883adea34d7ca62d63b5687b
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 15:51:29 +0500

    optimazed carimagetype

--- carimagetype.sql
+++ carimagetype.sql
@@ -3,3 +3,7 @@ CREATE TABLE car_image_types (
     image_type_id INTEGER NOT NULL REFERENCES image_type(id) ON DELETE CASCADE,
     PRIMARY KEY (car_image_id, image_type_id)
 );
+
+-- Optional: Add an index for the foreign keys to optimize query performance
+CREATE INDEX idx_car_image_types_car_image_id ON car_image_types (car_image_id);
+CREATE INDEX idx_car_image_types_image_type_id ON car_image_types (image_type_id);
