93d73601fb47477a948b2be376042baae86f43b4
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 18:41:29 +0500

    optimazed carcolor

--- arma_complex_SQL/carcolor.sql
+++ arma_complex_SQL/carcolor.sql
@@ -1,4 +1,5 @@
-SELECT car.name 
-FROM car 
+SELECT DISTINCT car.name
+FROM car
 JOIN car_color ON car.id = car_color.car_id
-WHERE car_color.color_1 = '#FFFFFF' OR car_color.color_2 = '#FFFFFF';
+WHERE '#FFFFFF' IN (car_color.color_1, car_color.color_2);
+
