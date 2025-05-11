f51e04706552f678bbeb0adb52d4c356469d9110
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 18:45:28 +0500

    optimazed old car and new car

--- arma_complex_SQL/oldnewcar.sql
+++ arma_complex_SQL/oldnewcar.sql
@@ -1,9 +1,13 @@
-SELECT name, manufacture_year 
-FROM car 
-ORDER BY manufacture_year ASC 
+SELECT name, manufacture_year
+FROM car
+WHERE manufacture_year IS NOT NULL
+ORDER BY manufacture_year ASC
 LIMIT 1;
 
-SELECT name, manufacture_year 
-FROM car 
-ORDER BY manufacture_year DESC 
+
+SELECT name, manufacture_year
+FROM car
+WHERE manufacture_year IS NOT NULL
+ORDER BY manufacture_year DESC
 LIMIT 1;
+
