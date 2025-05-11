1a8776284e2c846b71979f1fc6b9f491bc178f0a
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 15:53:17 +0500

    optimazed enginetype

--- enginetype.sql
+++ enginetype.sql
@@ -4,3 +4,6 @@ CREATE TABLE engine_type (
     slug_name VARCHAR(255) NOT NULL,
     image VARCHAR(255) NOT NULL
 );
+
+-- Optional: Add an index for the slug_name field to improve search performance
+CREATE INDEX idx_engine_type_slug_name ON engine_type (slug_name);
