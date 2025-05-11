0f543b1ae073e1e61edc6148c66dcbd913de2b52
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 18:44:33 +0500

    optimazed car price brand

--- arma_complex_SQL/carpricebrand.sql
+++ arma_complex_SQL/carpricebrand.sql
@@ -1,4 +1,4 @@
-SELECT brand.name, AVG(car.price) AS avg_price 
-FROM brand 
-JOIN car ON brand.id = car.brand_id 
-GROUP BY brand.name;
+SELECT brand.name, AVG(car.price) AS avg_price
+FROM brand
+JOIN car ON brand.id = car.brand_id
+GROUP BY brand.id, brand.name;
