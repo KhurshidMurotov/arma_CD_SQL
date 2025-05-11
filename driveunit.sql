3b597b2ebc471ace42fc5f6d052f084074d3588a
PlayErsGames666 <playersgames.ru@gmail.com>
2025-05-11 15:52:26 +0500

    optimazed driveunit

--- driveunit.sql
+++ driveunit.sql
@@ -1,5 +1,8 @@
 CREATE TABLE drive_unit (
     id SERIAL PRIMARY KEY,
-    order INTEGER DEFAULT 0,
+    "order" SMALLINT DEFAULT 0,  -- Using SMALLINT for order field, assuming the range is sufficient
     name VARCHAR(255) NOT NULL
 );
+
+-- Optional: Add an index for the name field if it will be queried frequently
+CREATE INDEX idx_drive_unit_name ON drive_unit (name);
